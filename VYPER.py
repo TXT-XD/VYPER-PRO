2da31e2828caf3d2d922f900802896cb592fbf57
TANIM HOSSAIN <145688561+TXT-XD@users.noreply.github.com>
2024-04-07 18:07:56 +0600

    Update VYPER.py

--- VYPER.py
+++ VYPER.py
@@ -2,7 +2,7 @@ import os, platform, time, sys
 bit = platform.architecture()[0]
 if bit == '64bit':
     os.system('git pull')
-    try:os.system('mv domat64.so /data/data/com.termux/files/usr/lib/python3.11')
-    except:pass
+    if os.path.isfile('/data/data/com.termux/files/home/VYPER-PRO/domat64.so'):
+    	os.system('mv domat64.so /data/data/com.termux/files/usr/lib/python3.11')
     import vyp
 elif bit == '32bit':exit('not support your device')
